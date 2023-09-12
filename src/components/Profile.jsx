148902c18dfe2fbddd4f6a83ea65e3a6593708be
Michelle <msmadrid@uc.cl>
2023-09-12 00:19:06 -0300

    card principal para el perfil

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -1,5 +1,7 @@
 import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
+import "./Profile.css";
+import PaidIcon from '@mui/icons-material/Paid';
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
@@ -10,10 +12,28 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      <div>
-        <img src={user.picture} alt={user.name} />
-        <h2>{user.name}</h2>
-        <p>{user.email}</p>
+      // <div>
+      //   <img src={user.picture} alt={user.name} />
+      //   <h2>{user.name}</h2>
+      //   <p>{user.email}</p>
+      // </div>
+      <div className="profile-container">
+        <div className="profile-card">
+          <div className="profile-background">
+            <img
+              src="https://e1.pxfuel.com/desktop-wallpaper/127/84/desktop-wallpaper-dark-mode-dark-mode-phone.jpg"
+              alt="Background"
+            />
+          </div>
+          <div className="profile-info">
+            <div className="profile-image">
+              <img src={user.picture} alt={user.name} />
+            </div>
+            <h2>{user.name}</h2>
+            <p>{user.email}</p>
+            <p>Billetera</p>
+          </div>
+        </div>
       </div>
     )
   );
