72d3fe0e2e59ce0bb250cd7d4e6fcd63fdf09d34
Michelle <msmadrid@uc.cl>
2023-09-20 18:46:52 -0300

    agrego botón de Billetera y Mostrar compras

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -2,9 +2,40 @@ import React from "react";
 import { useAuth0 } from "@auth0/auth0-react";
 import "./Profile.css";
 import PaidIcon from '@mui/icons-material/Paid';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
+import Modal from '@mui/material/Modal';
+import Box from '@mui/material/Box';
+
+const style = {
+  position: 'absolute',
+  top: '50%',
+  left: '50%',
+  transform: 'translate(-50%, -50%)',
+  width: 400,
+  bgcolor: 'background.paper',
+  border: '2px solid #000',
+  boxShadow: 24,
+  p: 4,
+};
 
 const Profile = () => {
   const { user, isAuthenticated, isLoading } = useAuth0();
+  const [open, setOpen] = React.useState(false);
+  const [showCompras, setShowCompras] = React.useState(false);
+  const [comprasButtonLabel, setComprasButtonLabel] = React.useState("Mostrar compras");
+
+  const handleOpen = () => setOpen(true);
+  const handleClose = () => setOpen(false);
+  const handleComprasClick = () => {
+    if (showCompras) {
+      setShowCompras(false);
+      setComprasButtonLabel("Mostrar compras");
+    } else {
+      setShowCompras(true);
+      setComprasButtonLabel("Ocultar compras");
+    }
+  };
 
   if (isLoading) {
     return <div>Loading ...</div>;
@@ -12,11 +43,6 @@ const Profile = () => {
 
   return (
     isAuthenticated && (
-      // <div>
-      //   <img src={user.picture} alt={user.name} />
-      //   <h2>{user.name}</h2>
-      //   <p>{user.email}</p>
-      // </div>
       <div className="profile-container">
         <div className="profile-card">
           <div className="profile-background">
@@ -31,9 +57,44 @@ const Profile = () => {
             </div>
             <h2>{user.name}</h2>
             <p>{user.email}</p>
-            <p>Billetera</p>
+            <div style={{ display: 'flex' }}>
+              <Button className="MuiButton-textPrimary" style={{ marginRight: '10px' }} onClick={handleOpen}>Billetera</Button>
+              <Button style={{ marginLeft: '10px' }} onClick={handleComprasClick}>{comprasButtonLabel}</Button>
+            </div>
+            <Modal
+              open={open}
+              onClose={handleClose}
+              aria-labelledby="modal-modal-title"
+              aria-describedby="modal-modal-description"
+            >
+              <Box sx={style}>
+                <Typography id="modal-modal-title" variant="h6" component="h2">
+                  Dinero disponible
+                </Typography>
+                <Typography id="modal-modal-description" sx={{ mt: 2 }}>
+                  Tu saldo actual es de: $__
+                </Typography>
+              </Box>
+            </Modal>
           </div>
         </div>
+        
+        {showCompras && (
+          <div className="compras-container">
+          <div className="compras-card">
+            <div className="compras-column">
+              <h3>Compras exitosas</h3>
+              {/* Add content for successful purchases */}
+            </div>
+          </div>
+          <div className="compras-card">
+            <div className="compras-column">
+              <h3>Compras pendientes</h3>
+              {/* Add content for pending purchases */}
+            </div>
+          </div>
+        </div>
+        )}
       </div>
     )
   );
