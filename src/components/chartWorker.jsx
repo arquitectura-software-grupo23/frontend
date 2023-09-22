9e6443f25befe5221e66522ccef64dfa4ed2ccf4
batfido <vicente.delfierro@uc.cl>
2023-09-22 00:13:09 -0300

    chartWorker

--- src/components/chartWorker.jsx
+++ src/components/chartWorker.jsx
@@ -1,62 +1,24 @@
-onmessage = function(e) {
-    const { data, interval } = e.data;
-  
-    function aggregateData(data, interval) {
-        let aggregated = [];
-        let currentTime = new Date(data[0].updatedAt).getTime(); // Assuming the data is sorted
-        let currentGroup = [];
-      
-        for (let entry of data) {
-          let entryTime = new Date(entry.updatedAt).getTime();
-          
-          // If the entry is within the current interval
-          if (entryTime - currentTime < interval) {
-            currentGroup.push(entry);
-          } else {
-            // Process the current group
-            if (currentGroup.length > 0) {
-              let open = currentGroup[0].price;
-              let close = currentGroup[currentGroup.length - 1].price;
-              let high = Math.max(...currentGroup.map(e => e.price));
-              let low = Math.min(...currentGroup.map(e => e.price));
-      
-              aggregated.push({
-                updatedAt: new Date(currentTime).toISOString(),
-                open,
-                close,
-                high,
-                low
-              });
-            }
-      
-            // Reset for next interval
-            while (entryTime - currentTime >= interval) {
-              currentTime += interval;
-            }
-            currentGroup = [entry];
-          }
-        }
-      
-        // Handle any remaining data
-        if (currentGroup.length > 0) {
-          let open = currentGroup[0].price;
-          let close = currentGroup[currentGroup.length - 1].price;
-          let high = Math.max(...currentGroup.map(e => e.price));
-          let low = Math.min(...currentGroup.map(e => e.price));
-      
-          aggregated.push({
-            updatedAt: new Date(currentTime).toISOString(),
-            open,
-            close,
-            high,
-            low
-          });
-        }
-      
-        return aggregated;
-      }
-      
-    const aggregatedData = aggregateData(data, interval);
-    postMessage(aggregatedData);
-  };
-  
+self.addEventListener('message', async (event) => {
+  const { action, symbol, chartSpanDays, candleSpanMins } = event.data;
+
+  if (action === 'fetch') {
+      const data = await fetchDataFromAPI(symbol, chartSpanDays, candleSpanMins);
+      // Return data back to the main thread
+      self.postMessage(data);
+  }
+});
+
+async function fetchDataFromAPI(symbol, chartSpanDays, candleSpanMins) {
+  // Construct the API endpoint URL based on the passed parameters. 
+  const url = `${import.meta.env.VITE_API_URL}/candlestick/${symbol}?chartSpanDays=${chartSpanDays}&candleSpanMins=${candleSpanMins}`;
+
+  try {
+      const response = await fetch(url);
+      const data = await response.json();
+      return data;
+  } catch (error) {
+      console.error("Error fetching data from API:", error);
+      // Handle errors as you see fit
+      self.postMessage({ error: "Error fetching data from API." });
+  }
+}
